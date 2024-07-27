a1716b96e8a90800dd6b1b02dd9403e5521d1890
Arseny Bulaev <seni190999@mail.ru>
2024-07-27 13:35:22 +0300

    set up

--- tdd/xUnit/main.py
+++ tdd/xUnit/main.py
@@ -2,6 +2,7 @@ from my_test import *
 
 def main():
     TestCaseTest("testRunning").run()
+    TestCaseTest("testSetUp").run()
 
 if __name__ == "__main__":
     main()
--- tdd/xUnit/my_test.py
+++ tdd/xUnit/my_test.py
@@ -1,22 +1,30 @@
 class testCase:
     def __init__(self, name) -> None:
         self.name = name
+    def setUp(self):
+        pass
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()
 
 
 class wasRun(testCase):
     def __init__(self, name) -> None:
-        self.wasRun = None
         testCase.__init__(self, name)
     def testMethod(self):
         self.wasRun = 1
+    def setUp(self):
+        self.wasRun = None
+        self.wasSetUp = 1
 
 
 class TestCaseTest(testCase):
+    def setUp(self):
+        self.test = wasRun("testMethod")
     def testRunning(self):
-        test = wasRun("testMethod")
-        assert(not test.wasRun)
-        test.run()
-        assert(test.wasRun)
+        self.test.run()
+        assert(self.test.wasRun)
+    def testSetUp(self):
+        self.test.run()
+        assert(self.test.wasSetUp)
