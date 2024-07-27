9c668dfac15f7d8c5d1be2523b6afdd29759d8c1
Arseny Bulaev <seni190999@mail.ru>
2024-07-27 14:52:49 +0300

    results report

--- tdd/xUnit/main.py
+++ tdd/xUnit/main.py
@@ -2,6 +2,8 @@ from my_test import *
 
 def main():
     TestCaseTest("testTemplateMethod").run()
+    TestCaseTest("testResult").run()
+    TestCaseTest("testFailedResult").run()
 
 if __name__ == "__main__":
     main()
--- tdd/xUnit/my_test.py
+++ tdd/xUnit/my_test.py
@@ -6,9 +6,12 @@ class testCase:
     def tearDown(self):
         pass
     def run(self):
+        result = TestResult()
+        result.testStarted()
         self.setUp()
         exec("self." + self.name + "()")
         self.tearDown()
+        return result
 
 
 class wasRun(testCase):
@@ -20,6 +23,8 @@ class wasRun(testCase):
         self.log = "setUp "
     def tearDown(self):
          self.log += "tearDown "
+    def testBrokenMethod(self):
+        raise Exception
 
 
 
@@ -30,3 +35,19 @@ class TestCaseTest(testCase):
         self.test = wasRun("testMethod")
         self.test.run()
         assert("setUp testMethod tearDown " == self.test.log)
+    def testResult(self):
+        self.test = wasRun("testMethod")
+        result = self.test.run()
+        assert("1 run, 0 failed" == result.summary())
+    def testFailedResult(self):
+        self.test = wasRun("testBrokenMethod")
+        result = self.test.run()
+        assert("1 run, 1 failed" == result.summary())
+
+class TestResult():
+    def __init__(self):
+        self.runCount = 0
+    def testStarted(self):
+        self.runCount += 1 
+    def summary(self):
+        return "%d run, 0 failed" % self.runCount
